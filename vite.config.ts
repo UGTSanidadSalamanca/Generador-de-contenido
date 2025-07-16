7c64c3be8b297e2da3f533bdf76b5b9765812026
UGTSanidadSalamanca <fespugtsalamanca@gmail.com>
2025-07-16 23:50:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Añade esta línea para que los paths de los assets sean relativos
+      base: './', 
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
